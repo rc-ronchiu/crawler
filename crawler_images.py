7bec36e616160157872f10d1a3a5b5a4078b4a16
rc-ronchiu <rc.ronchiu@gmail.com>
2020-04-02 19:56:47 +0800

    download the image

--- crawler_images.py
+++ crawler_images.py
@@ -1,11 +1,12 @@
 import os
+os.environ['http_proxy']=''
 import urllib.request as req
 import bs4
 
 url = str(input('URL: '))
 
 def getRawData(url):
-	request = req.Request(url, headers = {"User-Agent":"Mozilla"})
+	request = req.Request(url, headers = {"User-Agent":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:31.0) Gecko/20100101 Firefox/31.0"})
 	with req.urlopen(request) as response:
 		rawData = response.read().decode("utf-8")
 	return rawData
